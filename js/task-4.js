38e6ceeaad8e6ab77e5de00ac5a270c14b9af0c8
d-sofiika <soniadrozd2709@gmail.com>
2024-03-04 21:27:18 +0100

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -46,3 +46,55 @@ const clients = [
 console.log(getTotalBalanceByGender(clients, "male")); // 12053
 
 console.log(getTotalBalanceByGender(clients, "female")); // 8863
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class Admin extends User {
+  static role = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+
+  constructor({ email, access, blacklistedEmails}) {
+    super(email);
+    this.access = access;
+    this.blacklistedEmails = blacklistedEmails;
+  }
+
+  blacklist(email){
+     this.blacklistedEmails.push(email)
+  }
+  
+  isBlacklisted(email){
+    this.blacklistedEmails.includes()
+  }
+  
+}
+
+const mango = new Admin({
+  email: "mango@mail.com",
+  access: Admin.role.SUPERUSER,
+  blacklistedEmails: []
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.access); // "superuser"
+
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
